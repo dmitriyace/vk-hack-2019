410774df66623facb648595035c2409ec619a6af
Nick <nickkarmatskikh@gmail.com>
2019-09-27 21:13:08 +0300

    add starting server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/N1cOs/vkhack2019/server/grpc"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	const address = "127.0.0.1:8080"
+	server := grpc.NewServer()
+	if err := server.Serve(address); err != nil {
+		log.Fatal(err)
+	}
 }
