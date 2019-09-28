62e72dbe3be0e931ae1de01237b663b09b2273ec
Dmitriy <dmitriy.chistokhodov@emlid.com>
2019-09-28 14:27:14 +0300

    upd

--- client/lib/pages/sering/question_widgets/yes_dc_question.dart
+++ client/lib/pages/sering/question_widgets/yes_dc_question.dart
@@ -28,44 +28,40 @@ class YesDCQuestion extends StatefulWidget implements QuestWidget {
   _YesDCQuestionState createState() => _YesDCQuestionState();
 
   @override
-  void forward() async {
+  void back() async {
     var client = WeightsClient(this.channel);
-    if (this.question.continentWeights != null) {
+    if (this.prevQuestion.continentWeights != null) {
       var delta = ContinentDelta();
       delta.token = HomePage.token;
-      this.question.continentWeights[0].forEach((k, v) {
+      this.prevQuestion.continentWeights[0].forEach((k, v) {
         var d = Delta.create();
-        d.value = v;
+        d.value = -v;
         delta.targets[k] = d;
       });
       await client.changeContinent(delta);
     }
-    if (this.question.countryWeights != null) {
+    if (this.prevQuestion.countryWeights != null) {
       var delta = CountryDelta();
       delta.token = HomePage.token;
-      this.question.countryWeights[0].forEach((k, v) {
+      this.prevQuestion.countryWeights[0].forEach((k, v) {
         var d = Delta.create();
-        d.value = v;
+        d.value = -v;
         delta.targets[k] = d;
       });
       await client.changeCountry(delta);
     }
-    if (this.question.cityWeights != null) {
+    if (this.prevQuestion.cityWeights != null) {
       var delta = CityDelta();
       delta.token = HomePage.token;
-      this.question.cityWeights[0].forEach((k, v) {
+      this.prevQuestion.cityWeights[0].forEach((k, v) {
         var d = Delta.create();
-        d.value = v;
+        d.value = -v;
         delta.targets[k] = d;
       });
       await client.changeCity(delta);
     }
-<<<<<<< HEAD
     this.selectQuestionById(0, false);
     _YesDCQuestionState.model = false;
-=======
-    this.selectQuestionById(this.getNextQuestionId(), true);
->>>>>>> 4dfb77a0a6c8ef1fde4c3adc9ead93c621b1777b
   }
 
   void done() {
@@ -79,7 +75,6 @@ class YesDCQuestion extends StatefulWidget implements QuestWidget {
   }
 
   @override
-<<<<<<< HEAD
   void forward() async {
     var client = WeightsClient(this.channel);
     if (this.question.continentWeights != null) {
@@ -111,44 +106,6 @@ class YesDCQuestion extends StatefulWidget implements QuestWidget {
         delta.targets[k] = d;
       });
       await client.changeCity(delta);
-=======
-  void back() async {
-    if (_YesDCQuestionState.model) {
-      var client = WeightsClient(this.channel);
-      if (this.prevQuestion.continentWeights != null) {
-        var delta = ContinentDelta();
-        delta.token = HomePage.token;
-        this.prevQuestion.continentWeights[0].forEach((k, v) {
-          var d = Delta.create();
-          d.value = -v;
-          delta.targets[k] = d;
-        });
-        await client.changeContinent(delta);
-      }
-      if (this.prevQuestion.countryWeights != null) {
-        var delta = CountryDelta();
-        delta.token = HomePage.token;
-        this.prevQuestion.countryWeights[0].forEach((k, v) {
-          var d = Delta.create();
-          d.value = -v;
-          delta.targets[k] = d;
-        });
-        await client.changeCountry(delta);
-      }
-      if (this.prevQuestion.cityWeights != null) {
-        var delta = CityDelta();
-        delta.token = HomePage.token;
-        this.prevQuestion.cityWeights[0].forEach((k, v) {
-          var d = Delta.create();
-          d.value = -v;
-          delta.targets[k] = d;
-        });
-        await client.changeCity(delta);
-      }
-      this.selectQuestionById(0, false);
-    } else {
-      this.selectQuestionById(this.getNextQuestionId(), true);
->>>>>>> 4dfb77a0a6c8ef1fde4c3adc9ead93c621b1777b
     }
     this.selectQuestionById(this.getNextQuestionId(), true);
     _YesDCQuestionState.model = false;
