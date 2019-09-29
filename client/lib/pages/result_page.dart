7ca894242692984e1fb4529fec1c079e15752c9f
Dmitriy <dmitriy.chistokhodov@emlid.com>
2019-09-29 09:46:23 +0300

    prepare result page

--- client/lib/pages/result_page.dart
+++ client/lib/pages/result_page.dart
@@ -1,36 +1,33 @@
 import 'package:client/api/search.pb.dart';
 import 'package:flutter/material.dart';
 
-class ResultPage extends StatelessWidget  {
+class ResultPage extends StatelessWidget {
   ResultPage({Key key, this.results}) : super(key: key);
   final Cities results;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(child: Container(
-          width: 200,
+      body: Container(
+        color: Theme.of(context).backgroundColor,
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
           child: ListView.builder(
-              padding: const EdgeInsets.all(8),
               itemCount: this.results.values.length,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
                   height: 50,
-                  color: Colors.lightBlueAccent,
-                  child: Center(child: Row(
+                  child: Center(
+                      child: Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       Expanded(child: Text(this.results.values[index].name)),
-                      RaisedButton(
-                        child: Text('Перейти'),
-                        onPressed: null,
-                      )
                     ],
                   )),
                 );
-              }
-          ),
-        ),)
+              }),
+        ),
+      ),
     );
   }
 }
